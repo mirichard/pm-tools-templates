441a6b2640c0959e5eb052c448a8c4294e6fa690
Michael Richard <michaelrichard4@gmail.com>
2025-07-16 09:15:44 -0400

    feat: streamline Dashboard to use mock data, disable source maps, update README

--- Snowflake Demo/frontend/src/pages/Dashboard.js
+++ Snowflake Demo/frontend/src/pages/Dashboard.js
@@ -1,5 +1,6 @@
 // src/pages/Dashboard.js
 import React from 'react';
+import FilterPanel from '../components/FilterPanel';
 import GanttChart from '../components/GanttChart';
 import RiskMatrix from '../components/RiskMatrix';
 import KPIPanel from '../components/KPIPanel';
@@ -8,46 +9,21 @@ import ResourceHeatmap from '../components/ResourceHeatmap';
 import UpcomingTasks from '../components/UpcomingTasks';
 import { projectsData, milestonesData, tasksData, risksData, resourceLoadData } from '../data/mockData';
 
-import FilterPanel from '../components/FilterPanel';
-
-const Dashboard = () = {
-  const [filters, setFilters] = React.useState({ pms: [], status: [] });
-
-  const handleFilterChange = newFilters = {
-    setFilters(newFilters);
-  };
-
-  // TODO: apply filters to data sets
-
-  return (
-    <div style={{ padding: '16px' }}>
-      <FilterPanel onFilterChange={handleFilterChange} />
-      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '16px' }}>
-        <div style={{ gridColumn: '1 / span 2' }}>
-          <KPIPanel />
-        </div>
-        <GanttChart data={projectsData} />
-        <RiskMatrix data={risksData} />
-        <MilestoneTable data={milestonesData} />
-        <ResourceHeatmap data={resourceLoadData} />
-        <UpcomingTasks data={tasksData} />
+const Dashboard = () => (
+  <div style={{ padding: '16px' }}>
+    <FilterPanel onFilterChange={() => {}} />
+    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '16px' }}>
+      <div style={{ gridColumn: '1 / span 2' }}>
+        <KPIPanel />
       </div>
-    </div>
-  );
-};
-
-export default Dashboard;
-  <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '16px', padding: '16px' }}>
-    
-    <div style={{ gridColumn: '1 / span 2' }}>
-      <KPIPanel />
+      <GanttChart data={projectsData} />
+      <RiskMatrix data={risksData} />
+      <MilestoneTable data={milestonesData} />
+      <ResourceHeatmap data={resourceLoadData} />
+      <UpcomingTasks data={tasksData} />
     </div>
-    <GanttChart data={projectsData} />
-    <RiskMatrix data={risksData} />
-    <MilestoneTable data={milestonesData} />
-    <ResourceHeatmap data={resourceLoadData} />
-    <UpcomingTasks data={tasksData} />
   </div>
 );
 
 export default Dashboard;
+
